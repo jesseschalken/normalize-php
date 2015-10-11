cc72da6469fd86272879e886681d8d8905f7d66b
Jesse Schalken <me@jesseschalken.com>
2015-10-11 21:38:38 +1100

    Add code to copy semantic differences from one file to another

--- main.php
+++ main.php
@@ -3,6 +3,7 @@
 
 namespace NormalizePhp;
 
+use Exception;
 use PhpParser\Lexer;
 use PhpParser\Node;
 use PhpParser\Node\Name;
@@ -13,6 +14,148 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 ini_set('memory_limit', '-1');
 ini_set('xdebug.max_nesting_level', '100000');
+
+function node_props(Node $node) {
+    $props = [];
+    foreach ($node->getSubNodeNames() as $prop) {
+        $props[$prop] = $node->$prop;
+    }
+    return $props;
+}
+
+function array_flatten(array $foo) {
+    $flat = [];
+    foreach ($foo as $k => $v) {
+        if (is_array($v)) {
+            foreach (array_flatten($v) as $k1 => $v1) {
+                $flat["$k.$k1"] = $v1;
+            }
+        } else {
+            $flat[$k] = $v;
+        }
+    }
+    return $flat;
+}
+
+/**
+ * @param StringReplacements $string1
+ * @param Node               $node1
+ * @param Node               $node2
+ * @param string             $string2
+ */
+function replace_node(StringReplacements $string1, Node $node1, Node $node2, $string2) {
+    if (get_class($node1) !== get_class($node2)) {
+        $replace = true;
+    } else {
+        $props1 = array_flatten(node_props($node1));
+        $props2 = array_flatten(node_props($node2));
+
+        if (array_keys($props1) !== array_keys($props2)) {
+            $replace = true;
+        } else {
+            $replace = false;
+            foreach ($props1 as $k => $v1) {
+                $v2 = $props2[$k];
+                if (gettype($v1) !== gettype($v2)) {
+                    $replace = true;
+                    break;
+                } else if (is_scalar($v1) || is_null($v2)) {
+                    if ($v1 !== $v2) {
+                        $replace = true;
+                        break;
+                    }
+                } else if (
+                    $v1 instanceof Node &&
+                    $v2 instanceof Node
+                ) {
+                } else {
+                    $replace = true;
+                    break;
+                }
+            }
+
+            if (!$replace) {
+                foreach ($props1 as $k => $v1) {
+                    $v2 = $props2[$k];
+                    if (
+                        $v1 instanceof Node &&
+                        $v2 instanceof Node
+                    ) {
+                        replace_node($string1, $v1, $v2, $string2);
+                    }
+                }
+            }
+        }
+    }
+
+    if ($replace) {
+        $node1Start = $node1->getAttribute('startFilePos');
+        $node1End   = $node1->getAttribute('endFilePos') + 1;
+        $node2Start = $node2->getAttribute('startFilePos');
+        $node2End   = $node2->getAttribute('endFilePos') + 1;
+
+        $replacement = substr($string2, $node2Start, $node2End - $node2Start);
+        $string1->replace($node1Start, $node1End, $replacement);
+    }
+}
+
+class StringReplacement {
+    /** @var int */
+    public $length = 0;
+    /** @var string */
+    public $replacement = '';
+
+    /**
+     * @param int    $length
+     * @param string $replacement
+     */
+    function __construct($length, $replacement) {
+        $this->length      = $length;
+        $this->replacement = $replacement;
+    }
+}
+
+class StringReplacements {
+    /** @var string */
+    private $string;
+    /** @var StringReplacement[][] */
+    private $replacements = [];
+
+    /**
+     * @param string $string
+     */
+    function __construct($string) {
+        $this->string = $string;
+    }
+
+    /**
+     * @param int    $offsetStart
+     * @param int    $offsetEnd
+     * @param string $replacement
+     */
+    public function replace($offsetStart, $offsetEnd, $replacement) {
+        $this->replacements[$offsetStart][] = new StringReplacement($offsetEnd - $offsetStart, $replacement);
+    }
+
+    public function toString() {
+        ksort($this->replacements, SORT_NUMERIC);
+        $pos = 0;
+        $str = '';
+        foreach ($this->replacements as $offset => $replacements) {
+            foreach ($replacements as $replacement) {
+                if ($offset < $pos) {
+                    throw new Exception('Replacement overlap');
+                } else {
+                    $str .= substr($this->string, $pos, $offset - $pos);
+                    $pos = $offset + $replacement->length;
+                }
+                $str .= $replacement->replacement;
+            }
+        }
+        $str .= substr($this->string, $pos);
+        return $str;
+    }
+}
 
 /**
  * @param string $path
@@ -52,9 +195,14 @@ function find_php($file) {
     }
 }
 
+class UndoSemanticChanges {
+    function undo($oldFile, $newFIle) {
+    }
+}
+
 class Normalizer {
     /**
-     * @param Node[] $nodes
+     * @param Node[]   $nodes
      * @param callable $map
      * @return \PhpParser\Node[]
      */
@@ -80,7 +228,14 @@ class Normalizer {
     private $prettyPrinter;
 
     function __construct() {
-        $this->parser = (new ParserFactory)->create(ParserFactory::ONLY_PHP5, new Lexer());
+        $lexer = new Lexer([
+            'usedAttributes' => [
+                'startFilePos',
+                'endFilePos',
+            ],
+        ]);
+
+        $this->parser        = (new ParserFactory)->create(ParserFactory::ONLY_PHP5, $lexer);
         $this->prettyPrinter = (new Standard);
     }
 
@@ -92,9 +247,9 @@ class Normalizer {
         // Remove the hash-bang line if there, since
         // PhpParser doesn't support it
         if (substr($php, 0, 2) === '#!') {
-            $pos = strpos($php, "\n") + 1;
+            $pos      = strpos($php, "\n") + 1;
             $hashBang = substr($php, 0, $pos);
-            $php = substr($php, $pos);
+            $php      = substr($php, $pos);
         } else {
             $hashBang = '';
         }
@@ -104,7 +259,7 @@ class Normalizer {
         // Convert FALSE, TRUE and NULL into lowercase
         $nodes = self::mapNodes($nodes, function (Node $node) {
             if ($node instanceof Node\Expr\ConstFetch) {
-                $name = $node->name;
+                $name  = $node->name;
                 $lower = strtolower($name->toString());
                 if (
                     $lower === 'false' ||
@@ -114,12 +269,6 @@ class Normalizer {
                     $name->parts = array_map('strtolower', $name->parts);
                 }
             }
-            return $node;
-        });
-
-        // Remove comments
-        $nodes = self::mapNodes($nodes, function (Node $node) {
-            $node->setAttribute('comments', []);
             return $node;
         });
 
